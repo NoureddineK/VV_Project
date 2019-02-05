6052e31dc9ec675c329ca88fdd0db4a62452ca8b
fahimmerzouk <fahimovic94@gmail.com>
2019-02-05 20:48:40 +0100

    add assertion adder

---
+++ src/main/java/Software_Testing/VV_Project/AssertionAdder.java
@@ -0,0 +1,66 @@
+package Software_Testing.VV_Project;
+
+import org.junit.Assert;
+import spoon.reflect.code.CtExpression;
+import spoon.reflect.code.CtInvocation;
+import spoon.reflect.code.CtLocalVariable;
+import spoon.reflect.code.CtTypeAccess;
+import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.factory.Factory;
+import spoon.reflect.reference.CtExecutableReference;
+
+import java.util.List;
+import  Software_Testing.VV_Project.Observer;
+import static Software_Testing.VV_Project.Util.getGetters;
+import static Software_Testing.VV_Project.Util.getKey;
+import static Software_Testing.VV_Project.Util.invok;
+
+public class AssertionAdder {
+
+	private Factory factory;
+
+	public AssertionAdder(Factory factory) {
+		this.factory = factory;
+	}
+
+	@SuppressWarnings("unchecked")
+	public CtMethod addAssertion(CtMethod<?> testMethod, List<CtLocalVariable> ctLocalVariables) {
+		ctLocalVariables.forEach(ctLocalVariable -> this.addAssertion(testMethod, ctLocalVariable));
+		System.out.println("test method dans addassertion"+testMethod);
+		return testMethod;
+	}
+
+	@SuppressWarnings("unchecked")
+	public void addAssertion(CtMethod testMethod, CtLocalVariable localVariable) {
+		List<CtMethod> getters =
+				getGetters(localVariable);
+		getters.forEach(getter -> {
+			String key = getKey(getter);
+			CtInvocation invocationToGetter =
+					invok(getter, localVariable);
+			System.out.println("expected : "+factory.createLiteral(Observer.observations.get(key)));
+			System.out.println("ob : "+Observer.observations);
+			CtInvocation invocationToAssert = createAssert("assertEquals",
+					factory.createLiteral(Observer.observations.get(key)), // expected
+					invocationToGetter); // actual
+			System.out.println("-----------assertion added------------");
+			testMethod.getBody().insertEnd(invocationToAssert);
+		});
+		
+	}
+
+	public static CtInvocation createAssert(String name, CtExpression... parameters) {
+		final Factory factory = parameters[0].getFactory();
+		CtTypeAccess accessToAssert =
+				factory.createTypeAccess(factory.createCtTypeReference(Assert.class));
+		CtExecutableReference assertEquals = factory.Type().get(Assert.class)
+				.getMethodsByName(name).get(0).getReference();
+		return factory.createInvocation(
+				accessToAssert,
+				assertEquals,
+				parameters[0],
+				parameters[1]
+		);
+	}
+
+}
