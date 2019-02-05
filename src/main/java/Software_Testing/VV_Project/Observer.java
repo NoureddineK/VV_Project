737ead53fb9f4342b09c26946f43f9567a7daa92
fahimmerzouk <fahimovic94@gmail.com>
2019-02-05 20:47:41 +0100

    add observer of state of local variables

---
+++ src/main/java/Software_Testing/VV_Project/Observer.java
@@ -0,0 +1,12 @@
+package Software_Testing.VV_Project;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Observer {
+	public static Map<String, Object> observations = new HashMap<String, Object>();
+
+	public static void observe(String name, Object object) {
+		observations.put(name, object);
+	}
+}
