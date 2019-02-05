9bafde889a776b54ed3c754822603ba4349761fb
fahimmerzouk <fahimovic94@gmail.com>
2019-02-05 21:47:40 +0100

    add PersonTest

---
+++ src/test/java/classTested2/PersonTest.java
@@ -0,0 +1,34 @@
+package classTested2;
+
+import static org.junit.Assert.assertFalse;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import classToTest.Person;
+
+
+public class PersonTest {
+	@BeforeClass
+	public static void executedBeforeAllTests() {
+		System.out.println("No test executed yet");
+	}
+
+	@After
+	public void executedAfterEachMethod() {
+		System.out.println("Test finished");
+	}
+
+	@AfterClass
+	public static void executedAtTheEnd() {
+		System.out.println("All tests executed");
+	}
+
+	@Test
+	public void testAge() {
+		Person p = new Person("Someone", 13);
+		assertFalse(p.isAdult());
+	}
+}
